932b98c2db710412552a2fb547e6b16fe8f4663f
David Molineus <david.molineus@netzmacht.de>
2015-09-10 16:58:22 +0200

    Remove redirect after activation and clear the token.

--- src/FacebookConnect.php
+++ src/FacebookConnect.php
@@ -293,11 +293,8 @@ class FacebookConnect extends \TwigSimpleHybrid
 		// Log activity
 		$this->log('User account ID ' . $objMember->id . ' (' . $objMember->email . ') has been activated', __METHOD__, TL_ACCESS);
 
-		// Redirect to the jumpTo page
-		if (($objTarget = \PageModel::findByPk($this->facebook_connect_jumpTo)) !== null)
-		{
-			$this->redirect($this->generateFrontendUrl($objTarget->row()));
-		}
+		// Remove token so that only one module listen to it.
+		\Input::setGet('token', '');
 
 		// Confirm activation
 		$this->Template->type = 'confirm';
